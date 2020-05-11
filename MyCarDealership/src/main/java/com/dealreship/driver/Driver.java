7ac490615d8b5fe026518b8341502b3c7d2410f4
Raul Gonzalez <raulagonzalez@outlook.com>
2020-05-11 15:00:53 -0500

    udpated driver

--- MyCarDealership/src/main/java/com/dealreship/driver/Driver.java
+++ MyCarDealership/src/main/java/com/dealreship/driver/Driver.java
@@ -15,7 +15,17 @@ public class Driver {
 		String password = "Papuchi2013";
 		Connection conn = DriverManager.getConnection(JDBC_URL,user,password);
 		Statement st = conn.createStatement();
-		int n = st.executeUpdate("CREATE TABLE product(prod_id int, prod_name varchar2(30),prod_price int)");
+		int n = st.executeUpdate("CREATE TABLE customer(name varchar2(30), lastname varchar2(30),"
+				+ "username varchar2(30),password varchar2(30),custCarBrand varchar2(30),"
+				+ "custCarModel varchar2(30), custCarValue int, custRemainingPayments int,"
+				+ " lastFourSsnID int)");
+		int n1 = st.executeUpdate("Create Table offer (price varchar2(10),payment varchar2(10),"
+				+ "acceptedDenied varchar2(10),acceptedDenied2 varchar2(10),carId int)");
+		
+		int n2 = st.executeUpdate("CREATE TABLE car(price varchar2(20),model varchar2(20), price int, id int)");
+		
+		int n3 = st.executeUpdate("CREATE TABLE customercar(brand varchar(20),"
+				+ "model varchar(20),price int, id int, remPayments int, monPayment int)");
 		System.out.println("Number of rows affected"+n);
 	}
 
